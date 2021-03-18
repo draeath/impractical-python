a4dfe96239445b58b755a23b353583643495b04a
Paul Bransford <draeath@gmail.com>
2021-03-18 01:44:41 -0400

    word list cache

--- p02/words.py
+++ p02/words.py
@@ -1,4 +1,7 @@
 import re
+import base64
+import gzip
+import pickle
 
 
 def __is_duplicate_characters(string: str):
@@ -19,10 +22,20 @@ def __is_invalid_word(word: str, pattern: str = r'^[a-zA-Z]+$', minimum_length: 
 
 
 def get_words(filename: str = 'words'):
-    with open(filename) as file:
-        return_words = set()
-        for line in file.read().splitlines():
-            if __is_invalid_word(word=line):
-                continue
-            return_words.add(line.lower())
-    return sorted(return_words)
+    filename_b64 = base64.urlsafe_b64encode(filename.encode()).decode()
+    cache_filename = '.get_words__' + filename_b64
+    try:
+        with gzip.open(cache_filename, mode='rb') as file:
+            return_words = pickle.load(file)
+    except (pickle.UnpicklingError, gzip.BadGzipFile, EOFError, FileNotFoundError, PermissionError):
+        with open(filename) as file:
+            return_words = set()
+            for line in file.read().splitlines():
+                if __is_invalid_word(word=line):
+                    continue
+                return_words.add(line.lower())
+        return_words = sorted(return_words)
+    # no try/catch here, if it fails we want to know
+    with gzip.open(cache_filename, mode='wb', compresslevel=6) as file:
+        pickle.dump(return_words, file)
+    return return_words
