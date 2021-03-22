81b5e51e7b9e6e769631ea961a00ece539550648
Paul Bransford <paul.bransford@epi.usf.edu>
2021-03-22 17:34:43 -0400

    update

--- p02/words.py
+++ p02/words.py
@@ -1,7 +1,14 @@
+"""This module provides a caching method to access a words list with some basic filtering."""
+
 import re
 import base64
-import gzip
-import pickle
+import lzma
+import json
+import os.path
+
+
+class DataOutdatedError(Exception):
+    """Custom exception thrown when original is newer than cached data."""
 
 
 def __is_duplicate_characters(string: str):
@@ -14,20 +21,21 @@ def __is_duplicate_characters(string: str):
 
 
 def __is_invalid_word(word: str, pattern: str = r'^[a-zA-Z]+$', minimum_length: int = 2):
-    if (len(word) < minimum_length
-            or __is_duplicate_characters(word)
-            or re.fullmatch(pattern, word) is None):
+    if len(word) < minimum_length or __is_duplicate_characters(word) or re.fullmatch(pattern, word) is None:
         return True
     return False
 
 
 def get_words(filename: str = 'words'):
+    """Return a sorted and (somewhat) filtered wordlist from filename. The result is cached."""
     filename_b64 = base64.urlsafe_b64encode(filename.encode()).decode()
     cache_filename = '.get_words__' + filename_b64
     try:
-        with gzip.open(cache_filename, mode='rb') as file:
-            return_words = pickle.load(file)
-    except (pickle.UnpicklingError, gzip.BadGzipFile, EOFError, FileNotFoundError, PermissionError):
+        if os.path.getmtime(cache_filename) <= os.path.getmtime(filename):
+            raise DataOutdatedError
+        with lzma.open(cache_filename, mode='rt', encoding='utf8') as file:
+            return_words = json.load(file)
+    except (json.JSONDecodeError, lzma.LZMAError, EOFError, FileNotFoundError, PermissionError, DataOutdatedError):
         with open(filename) as file:
             return_words = set()
             for line in file.read().splitlines():
@@ -35,7 +43,7 @@ def get_words(filename: str = 'words'):
                     continue
                 return_words.add(line.lower())
         return_words = sorted(return_words)
-    # no try/catch here, if it fails we want to know
-    with gzip.open(cache_filename, mode='wb', compresslevel=6) as file:
-        pickle.dump(return_words, file)
+        with lzma.open(cache_filename, mode='wt', encoding='utf8',
+                       format=lzma.FORMAT_XZ, check=lzma.CHECK_SHA256, preset=lzma.PRESET_EXTREME) as file:
+            json.dump(return_words, file)
     return return_words
